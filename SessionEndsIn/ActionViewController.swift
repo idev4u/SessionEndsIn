84c063300e574ff6f9c213b84a52c863b189ae3e
idev4u <us.idev.student@googlemail.com>
2016-03-12 23:55:50 +0100

    add the logic for the timer

--- SessionEndsIn/ActionViewController.swift
+++ SessionEndsIn/ActionViewController.swift
@@ -11,24 +11,56 @@ import Foundation
 
 class ActionViewController: UIViewController {
     
-    @IBOutlet weak var EndInLabel: UILabel!
+   
+    @IBOutlet weak var endInLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     var timer = NSTimer()
+    var repeatCount = 1
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timer = NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: "updateLevel", userInfo: nil, repeats: true)
+        controlTimer(true)
         
     }
     
     func updateLevel(){
         NSLog("timer fired Action")
+        NSLog("the count is = \(repeatCount)")
+        
+        switch repeatCount {
+        case 1:
+            print("noch 5 Minuten")
+            endInLabel.text = "Noch 5 Minuten"
+        case 2:
+            print("that's all! finshed.")
+            finish()
+        default:
+            print("Something else")
+        }
+        
+        
+        repeatCount++
+        
         
     }
     
+    func controlTimer(repeats: Bool){
+        if(repeats){
+            timer = NSTimer.scheduledTimerWithTimeInterval(10.0, target: self, selector: "updateLevel", userInfo: nil, repeats: true)
+        }
+    }
+    
+    func finish(){
+        print("finshed is called ...")
+        timer.invalidate()
+        stopButton.setTitle("Finish", forState: UIControlState.Normal)
+        endInLabel.text = "that's all! finshed."
+    }
     
     @IBAction func stopAction() {
         timer.invalidate()
-        EndInLabel.text = "Reset"
+        endInLabel.text = "Reset"
     }
     
 }
